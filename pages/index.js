b99de18623838408cd52e7b1c14863f68d41cb42
zofi <zofi.src@gmail.com>
2022-07-12 17:12:50 -0500

    [IMP] Change introduction

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Ella es una chica desarrolladora con una personalidad multicolor ❤❤❤</p>
+        <p>Feliz códificando y escuchando rock ❤🎸❤❤</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
